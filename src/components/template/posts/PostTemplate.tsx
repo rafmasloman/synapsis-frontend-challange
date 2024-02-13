80fa632098f29307c6a9e8d977fb6e2d966d1000
rafmasloman <raflymasloman20@gmail.com>
2024-02-13 16:38:29 +0800

    fix: margin style on post

--- src/components/template/posts/PostTemplate.tsx
+++ src/components/template/posts/PostTemplate.tsx
@@ -4,7 +4,7 @@ import PostCard from '@/components/molecul/card/PostCard';
 
 const PostTemplate = ({ data }: PostTemplateTypes) => {
   return (
-    <section className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+    <section className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10">
       {data?.map((post) => {
         return (
           <PostCard
