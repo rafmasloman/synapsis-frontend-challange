fd354093a2dffb803b74e6d03c673d3b15c126e3
rafmasloman <raflymasloman20@gmail.com>
2024-02-14 17:45:21 +0800

    style: change search input style

--- src/components/molecul/input/SearchInput.tsx
+++ src/components/molecul/input/SearchInput.tsx
@@ -4,11 +4,11 @@ import { SearchInputPropsTypes } from './types/InputTypes';
 const SearchInput = ({
   searchQuery,
   handleSearchChange,
-  placeholder
+  placeholder,
 }: SearchInputPropsTypes) => {
   return (
     <input
-      className="bg-stone-100 shadow-sm px-5 py-2.5 rounded-lg w-full md:w-1/2 lg:w-1/3"
+      className="bg-stone-100 border  shadow-sm px-5 py-2.5 rounded-lg w-full md:w-1/2 lg:w-1/3"
       placeholder={placeholder}
       value={searchQuery}
       onChange={handleSearchChange}
